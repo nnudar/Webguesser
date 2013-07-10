5fc07b327bfabb3d138a7a7d54ced671ffb3bbc6
Nudar <nnudar@gmail.com>
2013-07-10 10:38:18 -0400

    Finished webguesser

--- webguesser.rb
+++ webguesser.rb
@@ -1,9 +1,24 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-number = (1 + rand(99)).to_s
+NUMBER = (1 + rand(99)).to_i
 
 get "/" do
-	erb :index, :locals => {:number => number}
+    guess = params["guess"] 
+    message = guessing(guess)
+
+	erb :index, :locals => {:number => NUMBER, 
+							:guess => guess, 
+							:message => message}	
+
 end
 
+ def guessing(guess)
+	    if guess.to_i > NUMBER
+	    	"Your guess is too high."
+	    elsif guess.to_i < NUMBER
+	    	"Your guess is too low."
+	    else guess.to_i == NUMBER
+	    	"Congratulations, you've won!"
+	   	end
+ end 
